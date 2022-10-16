65550eed8c7657b1bf32967eb012f7dc7a81cb57
Brett Houtz <bcat38@gmail.com>
2022-10-16 11:16:38 -0400

    react hooks and jsx

--- react.js
+++ react.js
@@ -1,4 +1,9 @@
 module.exports = {
-  'extends': ['./index', 'plugin:react/recommended'],
-  'plugins': ['react'],
+  'extends': ['./index', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  'plugins': ['react', 'react-hooks'],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    }
+  }
 };
