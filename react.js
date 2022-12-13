9a90be91e2c77aece73fa7c04644c2cc8abf1ffd
Brett Houtz <bcat38@gmail.com>
2022-12-12 21:03:32 -0500

    Use jsx-runtime in react config

--- react.js
+++ react.js
@@ -1,9 +1,14 @@
 module.exports = {
-  'extends': ['./index', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
-  'plugins': ['react', 'react-hooks'],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
-    }
-  }
+  extends: [
+    './index',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['react', 'react-hooks'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
 };
